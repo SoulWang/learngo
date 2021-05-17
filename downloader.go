813de526576d88a94b08654095eaea955f185896
SoulWang <442054828@qq.com>
2021-05-17 16:19:12 +0800

    learngo：第一次提交

---
+++ downloader.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://www.imooc.com")
+	if err != nil {
+		panic(err)
+
+	}
+	defer resp.Body.Close()
+	byte, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("%s\n", byte)
+
+}

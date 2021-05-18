e5367a25d9ea462844e1d42b6befae522491e688
SoulWang <442054828@qq.com>
2021-05-18 17:23:31 +0800

    阶段二（2-1）：ending,,,

--- functional/adder/adder.go
+++ functional/adder/adder.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+//闭包：函数体{局部变量；自由变量}
 func adder() func(int) int {
 	sum := 0
 	return func(v int) int {
@@ -12,9 +13,19 @@ func adder() func(int) int {
 	}
 }
 
+type iAdder func(int) (int, iAdder)
+
+func adder2(base int) iAdder {
+	return func(v int) (int, iAdder) {
+		return base + v, adder2(base + v)
+	}
+}
+
 func main() {
-	a := adder()
+	a := adder2(0)
 	for i := 0; i < 10; i++ {
-		fmt.Println(a(i))
+		var s int
+		s, a = a(i)
+		fmt.Printf("0 + 1 +  ... + %d = %d\n", i, s)
 	}
 }
